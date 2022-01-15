052b3cf91d14272de91e1f5e506d129b63439560
Aaron Wolen <aaron@wolen.com>
2022-01-15 19:33:45 +0000

    Rewrite CardList class
    - now uses json instead of csv
    - on-disk format is compatible with magic-cards

--- CardList.py
+++ CardList.py
@@ -1,37 +1,82 @@
-import csv
+from typing import List, Optional
+import json
+import uuid
 import os.path
-import sys
 
 
 class CardList:
-    def __init__(self):
-        self.path = os.path.dirname(os.path.realpath(__file__))
-        self.cardList = self.readList()
+    """
+    Initialize CardList class
 
-    def readList(self):
-        with open(self.path + "/cardList.csv", mode="r") as infile:
-            reader = csv.reader(infile)
-            cardList = {rows[0]: rows[1] for rows in reader}
-            infile.close()
-        return cardList
+    On-disk format of cards.json designed to be compatible with:
+    https://github.com/maddox/magic-cards.
+    """
 
-    def getPlaylist(self, card):
-        self.cardList = self.readList()
-        try:
-            return self.cardList[card]
-        except:
-            print(f"Card {card} is not in card list")
-            return ""
+    def __init__(self, config_dir: str = "config"):
+        # store card list as dictionary of dictionaries indexed by card code
+        self.__cards = {}
+        self.config_dir = config_dir
+        self.card_list_file = os.path.join(self.config_dir, "cards.json")
+        self.card_list_file_exists = os.path.isfile(self.card_list_file)
 
-    def addPlaylist(self, card, plist):
-        try:
-            if card not in self.cardList.keys():
-                f = open(self.path + "/cardList.csv", "a")
-                f.write(card + "," + plist + "\n")
-                self.cardList[card] = plist
-            else:
-                print(f"Card {card} is already used")
-        except:
-            print("Could not write file")
-            if not os.path.isfile(self.path + "/cardList.csv"):
-                print("File cardList.csv does not exist")
+        if not self.card_list_file_exists:
+            raise FileNotFoundError("Card list file not found")
+        else:
+            self.read_card_list()
+
+    def get_card_list_file(self) -> str:
+        """Get card list file path"""
+        return self.card_list_file
+
+    def get_card_list_file_exists(self) -> bool:
+        """Check that card list file exists"""
+        return self.card_list_file_exists
+
+    def read_card_list(self) -> None:
+        """Read card list from json file"""
+        with open(self.card_list_file, "r") as f:
+            self.__cards = {card["code"]: card for card in json.load(f)}
+            f.close()
+
+    def get_card_list(self) -> List[dict]:
+        return [card for card in self.__cards.values()]
+
+    def get_card(self, code: str) -> dict:
+        """
+        Get card by card code
+        """
+        return self.__cards.get(code)
+
+    def add_card(
+        self,
+        code: str,
+        uri: str,
+        card_type: str,
+        title: Optional[str] = None,
+        subtitle: Optional[str] = None,
+        art_url: Optional[str] = None,
+    ) -> None:
+        """
+        Add new card to card list
+        """
+
+        assert card_type in ["song", "album", "playlist", "event"]
+        card = {
+            "code": code,
+            "type": card_type,
+            "action": "Home Assistant",
+            "artURL": art_url,
+            "title": title,
+            "subtitle": "",
+            "uri": uri,
+            "id": str(uuid.uuid1()),
+        }
+
+        self.__cards.update({card["code"]: card})
+        self.write_card_list()
+
+    def write_card_list(self) -> None:
+        """Write card list to json file"""
+        with open(self.card_list_file, "w") as f:
+            json.dump(self.get_card_list(), f, indent=2)
+            f.close()
